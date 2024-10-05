a0553d0abbcbab00b09728f8cd4b91db46086fd3
AleGod125 <alejandroviolori@gmail.com>
2024-10-05 10:24:18 -0500

    casi terminado

--- ui/inventario/inventario.js
+++ ui/inventario/inventario.js
@@ -2,6 +2,10 @@ import productosCarnes from '/data/fakebd.js';
 
 const contenedor = document.getElementById("productos-container");
 
+window.Click = function(tipo) {
+    window.location.href = `/ui/inventarioDetallado/inventarioDetallado.html?tipo=${encodeURIComponent(tipo)}`;
+};
+
 productosCarnes.forEach(producto => {
     const cardHTML = `
         <div class="card A" style="width: 18rem;">
@@ -9,7 +13,7 @@ productosCarnes.forEach(producto => {
             <div class="card-body">
                 <h5 class="card-title" style="color: black;">${producto.nombre}</h5>
                 <p class="card-text" style="color: black;">${producto.descripcion}</p>
-                <a href="#" class="btn btn-primary">Ver más</a>
+                <a href="#" onclick="Click('${producto.tipo}')" class="btn btn-primary">Ver más</a>
             </div>
         </div>
     `;
---
+++ ui/inventarioDetallado/inventarioDetallado.js
@@ -0,0 +1,65 @@
+import productosCarnes, { carnesDeRes, carnesDePollo, carnesDeCerdo } from '/data/fakebd.js';
+
+function getQueryParam(param) {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(param);
+}
+
+const tipoTexto = document.getElementById('tipoTexto');
+let tipo = getQueryParam('tipo');
+if (!tipo) {
+    tipo = 'Res'; 
+}
+tipoTexto.textContent = `Carne de: ${tipo}`;
+
+const idsContainer = document.getElementById('ids');
+const namesContainer = document.getElementById('names');
+const cantidadContainer = document.getElementById('cantidad');
+const precioContainer = document.getElementById('precio');
+const ingresoContainer = document.getElementById('ingreso');
+const vencimientoContainer = document.getElementById('vencimiento');
+
+let carnes;
+switch (tipo) {
+    case 'Res':
+        carnes = carnesDeRes;
+        break;
+    case 'Pollo':
+        carnes = carnesDePollo;
+        break;
+    case 'Cerdo':
+        carnes = carnesDeCerdo;
+        break;
+    default:
+        console.error('Tipo de carne no válido');
+        break;
+}
+
+if (carnes) {
+    carnes.forEach(carne => {
+        const idElement = document.createElement('div');
+        idElement.textContent = carne.id;
+        
+        const nameElement = document.createElement('div');
+        nameElement.textContent = carne.nombre;
+        
+        const cantidadElement = document.createElement('div');
+        cantidadElement.textContent = carne.cantidad +" kg";
+        
+        const precioElement = document.createElement('div');
+        precioElement.textContent = `${carne.precio/2} COP`;
+        
+        const ingresoElement = document.createElement('div');
+        ingresoElement.textContent = carne.FechaIngreso;
+        
+        const vencimientoElement = document.createElement('div');
+        vencimientoElement.textContent = carne.FechaVencimiento;
+
+        idsContainer.appendChild(idElement);
+        namesContainer.appendChild(nameElement);
+        cantidadContainer.appendChild(cantidadElement);
+        precioContainer.appendChild(precioElement);
+        ingresoContainer.appendChild(ingresoElement);
+        vencimientoContainer.appendChild(vencimientoElement);
+    });
+}
