1b721c69bfeab11c9cfb0dee5f67f4de33e2c543
AleGod125 <alejandroviolori@gmail.com>
2024-10-05 18:20:52 -0500

    Update faturacion.js

--- ui/facturacion/faturacion.js
+++ ui/facturacion/faturacion.js
@@ -1,5 +1,5 @@
 import productosCarnes, { carnesDeRes, carnesDePollo, carnesDeCerdo } from '../../data/fakebd.js';
-import { agregarElemento, eliminarElemento, obtenerListaCompra } from '..listaCompraServide.js';
+import { agregarElemento, eliminarElemento, obtenerListaCompra } from '../../domain/listaCompraServide.js';
 
 const searchInput = document.getElementById('searchInput');
 const suggestionsContainer = document.getElementById('suggestions');
