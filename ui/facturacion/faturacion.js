39f4a25de429c11e988ea79904c548463bccb7b6
AleGod125 <alejandroviolori@gmail.com>
2024-10-05 19:49:48 -0500

    funciona en mi pc
    PUTA MADRE LLEVO 20 COMMIT Y NO LOGRADO ENTENDER EL ERROR :"V

--- ui/facturacion/faturacion.js
+++ ui/facturacion/faturacion.js
@@ -1,5 +1,5 @@
-import productosCarnes, { carnesDeRes, carnesDePollo, carnesDeCerdo } from './data/fakebd.js'; // Ruta relativa
-import { agregarElemento, eliminarElemento, obtenerListaCompra } from './domain/listaCompraService.js'; // Ruta relativa
+import productosCarnes, { carnesDeRes, carnesDePollo, carnesDeCerdo } from '/data/fakebd.js'; // Ruta relativa
+import { agregarElemento, eliminarElemento, obtenerListaCompra } from '/domain/listaCompraServide.js'; // Ruta relativa
 
 const searchInput = document.getElementById('searchInput');
 const suggestionsContainer = document.getElementById('suggestions');
